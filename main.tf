3c328314b9b2defc2875a40d3586dc5f4435e691
Ales Lerch <13370338+axeII@users.noreply.github.com>
2024-02-13 02:46:24 +0100

    fix: planing pipeline

--- main.tf
+++ main.tf
@@ -15,9 +15,7 @@ terraform {
 }
 
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.region
+  region = var.region
 }
 
 data "aws_ami" "ubuntu" {
--- variables.tf
+++ variables.tf
@@ -1,10 +1,11 @@
-variable "aws_access_key" {
-  type = string
-}
+# TODO: Delete these only for local testing
+# variable "aws_access_key" {
+#   type = string
+# }
 
-variable "aws_secret_key" {
-  type = string
-}
+# variable "aws_secret_key" {
+#   type = string
+# }
 
 variable "region" {
   default = "eu-west-1"
